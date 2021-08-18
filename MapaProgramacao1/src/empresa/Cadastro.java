68e6780f65edd2896eb215e33d5ae1e08e675216
jccesar22 <60848753+jccesar22@users.noreply.github.com>
2021-08-17 23:05:17 -0300

    Commit
    arquivos para o mapa da faculdade

---
+++ MapaProgramacao1/src/empresa/Cadastro.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package empresa;
+
+/**
+ *
+ * @author cesar
+ */
+public class Cadastro {
+    public String nome;
+    public int unitario;
+    public String unidade;
+    public double estoque;
+
+    public Cadastro() {
+    }
+    public Cadastro(String nome, int unitario, String unidade, double estoque) {
+        this.nome = nome;
+        this.unitario = unitario;
+        this.unidade = unidade;
+        this.estoque = estoque;
+    }
+    
+    
+    
+    
+}
---
+++ MapaProgramacao1/src/mapaprogramacao1/MapaProgramacao1.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mapaprogramacao1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author cesar
+ */
+public class MapaProgramacao1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        int op;
+        do {
+            System.out.println("**************************************MAMU**************************************");
+            System.out.println("1 - CADASTRO DE PRODUTOS\n"
+                    + "2 - MOVIMENTAÇÃO\n"
+                    + "3 - REAJUSTE DE PREÇOS\n"
+                    + "4 - RELATÓRIOS\n"
+                    + "0 - FINALIZAR\n"
+                    + "OPÇÃO: _");
+            switch (op) {
+                case 1:
+                    break;
+                default:
+                    System.out.println("Opção invalida digite novamente");
+                    break;
+            }
+            
+        }
+        while (op != 0);
+
+    }
